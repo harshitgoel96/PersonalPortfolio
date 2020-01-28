bd0862bbd139fc10039b6be1134967d39a026b96
Harshit From Home <harshitgoel96@yahoo.com>
2020-01-28 19:36:21 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ServicesProvidedComponent} from './services-provided/services-provided.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  { path: 'services',component:ServicesProvidedComponent  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesProvidedComponent } from './services-provided/services-provided.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule,MatSidenavModule} from '@angular/material';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicesProvidedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,MatIconModule,MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
